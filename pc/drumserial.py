d5645da82608bd054e01b7b4bf40164e5187cbd6
Nick Brown <nfbrown@wpi.edu>
2014-09-07 14:20:32 -0400

    add serial read to drumserial.py, add this functionality to test.py

--- pc/drumserial.py
+++ pc/drumserial.py
@@ -2,15 +2,21 @@ import serial
 import numpy as np
 
 class DrumSerial:
-    serial_out = serial.Serial()
+    serial_port = serial.Serial()
 
     def __init__(self, port):
-        self.serial_out.baudrate = 115200
-        self.serial_out.bytesize = 32
-        self.serial_out.open(port)
+        self.serial_port.baudrate = 115200
+        self.serial_port.bytesize = 32
+        self.serial_port.open(port)
 
     def send(self, packet):
-        self.serial_out.write(packet)
+        self.serial_port.write(packet)
+
+    def receive(self):
+        self.serial_port.read(4)
+
+    def close(self):
+        self.serial_port.close()
 
 def create_packet(sequence, reset, control, pps, drums):
     packet = (sequence & 0x3) << 30
@@ -19,3 +25,12 @@ def create_packet(sequence, reset, control, pps, drums):
     packet |= (pps & 0xFF) << 16
     packet |= (drums & 0xFFFF)
     return np.uint32(packet)
+
+
+def parse_packet(packet):
+    seq = (packet >> 30) & 0x3
+    rst = (packet >> 29) & 0x1
+    con = (packet >> 25) & 0xF
+    pps = (packet >> 16) & 0xFF
+    drm = (packet & 0xFFFF)
+    return (seq, rst, con, pps, drm)
--- pc/test.py
+++ pc/test.py
@@ -1,14 +1,19 @@
 import drumserial as dp
 import sys
+import time
 
 
 def main():
     if len(sys.argv) == 2:
-        s = dp.SendPacket(sys.argv[1])
-        s.send(dp.create_packet(0, 0, 0, 0, 0))
-        s.send(dp.create_packet(1, 1, 1, 1, 1))
-        s.send(dp.create_packet(2, 2, 2, 2, 2))
-        s.send(dp.create_packet(3, 3, 3, 3, 3))
+        s = dp.DrumSerial(sys.argv[1])
+        for i in range(4):
+            s.send(dp.create_packet(i, i, i, i, i))
+            time.sleep(0.1)
+            r = dp.parse_packet(s.receive())
+            if (r != (i, i, i, i, i)):
+                print "Error: sent packet does not match received packet"
+                break
+        s.close()
     else:
         print "Error: enter a port"
 
