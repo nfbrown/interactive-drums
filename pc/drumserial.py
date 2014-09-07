2bc61375847d48e42f65f9b9a056542caab64ade
Nick Brown <nfbrown@wpi.edu>
2014-09-07 15:48:21 -0400

    add serial port testing programs

--- pc/drumserial.py
+++ pc/drumserial.py
@@ -1,23 +1,30 @@
 import serial
 import numpy as np
 
+
 class DrumSerial:
     serial_port = serial.Serial()
 
     def __init__(self, port):
         self.serial_port.baudrate = 115200
-        self.serial_port.bytesize = 32
-        self.serial_port.timeout = 15
-        self.serial_port.open(port)
+        self.serial_port.timeout = 5
+        self.serial_port.port = port
+        self.serial_port.open()
 
     def send(self, packet):
         self.serial_port.write(packet)
 
+    def readline(self):
+        return self.serial_port.readline()
+
     def receive(self):
-        self.serial_port.read(4)
+        b = bytearray(4)
+        self.serial_port.readinto(b)
+        return b
 
     def close(self):
         self.serial_port.close()
+
 
 def create_packet(sequence, reset, control, pps, drums):
     packet = (sequence & 0x3) << 30
@@ -25,13 +32,16 @@ def create_packet(sequence, reset, control, pps, drums):
     packet |= (control & 0xF) << 25
     packet |= (pps & 0xFF) << 16
     packet |= (drums & 0xFFFF)
-    return np.uint32(packet)
+    b = bytearray(4)
+    for i in range(4):
+        b[i] = packet >> (i*8) & 0xFF
+    return b
 
 
 def parse_packet(packet):
-    seq = (packet >> 30) & 0x3
-    rst = (packet >> 29) & 0x1
-    con = (packet >> 25) & 0xF
-    pps = (packet >> 16) & 0xFF
-    drm = (packet & 0xFFFF)
+    seq = (packet[3] >> 6) & 0x3
+    rst = (packet[3] >> 5) & 0x1
+    con = (packet[3] >> 1) & 0xF
+    pps = (packet[2] & 0x7F)
+    drm = (int(packet[1]) << 8) | packet[0]
     return (seq, rst, con, pps, drm)
--- pc/test.py
+++ pc/test.py
@@ -1,16 +1,16 @@
 import drumserial as dp
 import sys
-import time
 
 
 def main():
     if len(sys.argv) == 2:
         s = dp.DrumSerial(sys.argv[1])
-        for i in range(4):
-            s.send(dp.create_packet(i, i, i, i, i))
-            time.sleep(0.1)
+        for i in range(0, 4):
+            s.send(dp.create_packet(i, i % 2, i, i, i))
+            print "Sent packet: (%d, %d, %d, %d, %d)" % (i, i % 2, i, i, i)
             r = dp.parse_packet(s.receive())
-            if (r != (i, i, i, i, i)):
+            print "Received packet: ", r
+            if (r != (i, i % 2, i, i, i)):
                 print "Error: sent packet does not match received packet"
                 break
         s.close()
