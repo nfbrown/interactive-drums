73d951b1dec3ef88ea8ed1f8a34db216653d564e
Nick Brown <nfbrown@wpi.edu>
2014-10-07 23:55:49 -0400

    update to new mido and add time conversion

--- pc/midiparser.py
+++ pc/midiparser.py
@@ -3,11 +3,30 @@ import mido
 
 
 def midi_to_packets(filename):
-    messages = [i for i in mido.MidiFile(filename) if type(i) == mido.Message
-                and i.type == 'note_on'
-                and i.velocity != 0]
-    m = [(i.note, i.time) for i in messages]
-    print m
+    mf = mido.MidiFile(filename)
+    ticks_per_beat = mf.ticks_per_beat
+    seconds_per_beat = 0.5
+    tempo_list = [i for i in mf
+                  if type(i) == mido.MetaMessage and i.type == 'tempo']
+    if tempo_list != []:
+        seconds_per_beat = tempo_list[0].tempo / 1000000.0
+
+    seconds_per_tick = seconds_per_beat / ticks_per_beat
+    note_ons = [i for i in mf if type(i) == mido.Message
+                and i.type == 'note_on']
+    meta_messages = [i for i in mf
+                     if type(i) == mido.MetaMessage]
+    m = [(i.note, i.velocity, float(i.time)) for i in note_ons]
+    print delta_time_to_seconds(m, seconds_per_tick)
+
+
+def delta_time_to_seconds(note_on_tuples, seconds_per_tick):
+    total = float(0.0)
+    new_tuples = []
+    for i in note_on_tuples:
+        total += i[2]
+        new_tuples.append((i[0], i[1], total))
+    return [i for i in new_tuples if i[1] != 0]
 
 
 def main():
